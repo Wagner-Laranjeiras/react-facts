16e50a83ceb4d12541da49091b95e98a7cc8b82c
Wagner-Laranjeiras <wlaranjeiras.dev@gmail.com>
2025-03-14 12:08:19 +0100

    ending day commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ const root: Root = createRoot(document.querySelector('#root') as HTMLBodyElement
 
 const MainContent =  () => <h1>React is Great!</h1>
 
-root.render(
-  // <ul>
-  //   <li>I will feel more confident with my coding skills.</li>
-  //   <li>It's semms really good to get a job.</li>
-  //   <li>I'm building an app with React.</li>
-  // </ul>
+const listEl = document.querySelector('#list');
 
-  <MainContent />
+
+const newHeaderElement = document.createElement('h1');
+newHeaderElement.textContent = 'Im a vanilla text within a H1';
+newHeaderElement.className = 'new-class'
+document.querySelector('#root').appendChild(newHeaderElement)
+
+root.render(
+  <>
+    <MainContent />
+  
+  </>
 );

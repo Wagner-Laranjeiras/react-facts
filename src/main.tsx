fb88f64770b6edad511c3d841877f25295272be9
Wagner-Laranjeiras <wlaranjeiras.dev@gmail.com>
2025-03-14 11:43:30 +0100

    rendering a list in React

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot, Root } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root: Root = createRoot(document.querySelector('#root') as HTMLBodyElement);
+
+root.render(
+  <ul>
+    <li>I will feel more confident with my coding skills.</li>
+    <li>It's semms really good to get a job.</li>
+    <li>I'm building an app with React.</li>
+  </ul>
+);

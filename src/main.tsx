a56c9418b71f87bb89a2f3c3a228ea14dd06929d
Wagner-Laranjeiras <wlaranjeiras.dev@gmail.com>
2025-03-14 11:54:47 +0100

    First custom component

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { createRoot, Root } from "react-dom/client";
 
 const root: Root = createRoot(document.querySelector('#root') as HTMLBodyElement);
 
+const MainContent =  () => <h1>React is Great!</h1>
+
 root.render(
-  <ul>
-    <li>I will feel more confident with my coding skills.</li>
-    <li>It's semms really good to get a job.</li>
-    <li>I'm building an app with React.</li>
-  </ul>
+  // <ul>
+  //   <li>I will feel more confident with my coding skills.</li>
+  //   <li>It's semms really good to get a job.</li>
+  //   <li>I'm building an app with React.</li>
+  // </ul>
+
+  <MainContent />
 );
